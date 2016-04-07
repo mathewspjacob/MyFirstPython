0c61d66745a1b72c7710251bcc6d5144304499dc
Mathews P Jacob <Mathews P Jacob@MATHEWS>
2016-04-07 22:37:47 +0530

    Add project files.

---
+++ MyFirstPython/MyFirstPython.py
@@ -0,0 +1,37 @@
+#printing something to screen
+print('#printing something to screen\n----------------------------')
+print("hellow world")
+print("It's")
+print("hai this is\n\tMathews")
+print("""This is another 
+         way for printing lines
+         in multiple line""")
+print('''This is another 
+         way for printing lin
+         in multiple line''')
+
+print("\n")
+#printing URL
+print('#printing URL\n-----------------')
+print("www.google.com\\index.php")
+print("\n\n")
+
+#reading input from user
+print('#reading input from user\n-----------------------')
+name = input("what is ur name?")
+print("Hai "+name)
+print("\n\n")
+
+#upper case, loawer case and swap
+print('#upper case, loawer case and swap\n---------------------------------')
+meaasage = "Python programming is Awesome!"
+print(meaasage.lower())
+print(meaasage.upper())
+print(meaasage.swapcase())
+print("\n\n")
+
+#function will do
+print('#function will do\n-----------------')
+print(meaasage.capitalize())
+print(meaasage.count('m'))
+print(meaasage.count("Awesome"))
